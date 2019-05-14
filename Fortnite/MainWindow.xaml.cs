2f1c52c66e82c4fcc1c68aacbcaa03998aef9355
Naserte sidementi <rokluk@seznam.cz>
2019-05-14 15:01:07 +0200

    Přidejte soubory projektu.

---
+++ Fortnite/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fortnite {
+    public partial class MainWindow : Window {
+        private JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+        List<Shop> items = new List<Shop>();
+        public MainWindow() {
+            InitializeComponent();
+            loadall();
+        }
+
+        async void loadall() {
+            HttpClient httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://fortnite-public-api.theapinetwork.com/prod09/store/get");
+            string jsonContent = await response.Content.ReadAsStringAsync();
+            items = JsonConvert.DeserializeObject<List<Shop>>(jsonContent, settings);
+            MessageBox.Show(items.ToString());
+        }
+    }
+}
---
+++ Fortnite/Shop.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fortnite {
+    class Shop {
+        public string vbucks { get; set; }
+        public string date_layout { get; set; }
+        public string date { get; set; }
+        public int lastupdate { get; set; }
+        public string language { get; set; }
+        public int rows { get; set; }
+        public List<Item> items { get; set; }
+    }
+
+        public class Featured {
+            public string transparent { get; set; }
+        }
+
+        public class Images {
+            public string transparent { get; set; }
+            public string background { get; set; }
+            public string information { get; set; }
+            public Featured featured { get; set; }
+        }
+
+        public class Item2 {
+            public string name { get; set; }
+            public string description { get; set; }
+            public string image { get; set; }
+            public Images images { get; set; }
+            public string captial { get; set; }
+            public string type { get; set; }
+            public string rarity { get; set; }
+            public string obtained_type { get; set; }
+        }
+
+        public class Ratings {
+            public double avgStars { get; set; }
+            public int totalPoints { get; set; }
+            public int numberVotes { get; set; }
+        }
+
+        public class Item {
+            public string itemid { get; set; }
+            public string name { get; set; }
+            public string cost { get; set; }
+            public int featured { get; set; }
+            public int refundable { get; set; }
+            public int lastupdate { get; set; }
+            public Item2 item { get; set; }
+            public Ratings ratings { get; set; }
+        }
+    }
