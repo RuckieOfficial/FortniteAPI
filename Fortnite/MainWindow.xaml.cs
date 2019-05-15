e99613e5aeeba35a6b540d101aedff98e2ea8bc5
Rückie <Rckie@DESKTOP-HJ23VHO>
2019-05-15 13:59:02 +0200

    init

--- Fortnite/MainWindow.xaml.cs
+++ Fortnite/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Shapes;
 namespace Fortnite {
     public partial class MainWindow : Window {
         private JsonSerializerSettings settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All };
+        Shop items = new Shop();
         public MainWindow() {
             InitializeComponent();
             loadall();
@@ -27,9 +28,78 @@ namespace Fortnite {
             HttpClient httpClient = new HttpClient();
             var response = await httpClient.GetAsync("https://fortnite-public-api.theapinetwork.com/prod09/store/get");
             string jsonContent = await response.Content.ReadAsStringAsync();
-            Shop items = JsonConvert.DeserializeObject<Shop>(jsonContent, settings);
+            items = JsonConvert.DeserializeObject<Shop>(jsonContent, settings);
 
-            MessageBox.Show(items.vbucks.ToString());
+            addall();
+            //MessageBox.Show(items.vbucks.ToString());
+        }
+
+        void addall() {
+            foreach(Item item in items.items) {
+                BitmapImage bmpitemimage = new BitmapImage();
+                bmpitemimage.BeginInit();
+                bmpitemimage.UriSource = new Uri(item.item.image);
+                bmpitemimage.EndInit();
+
+                Image itemimage = new Image();
+                itemimage.Source = bmpitemimage;
+                itemimage.Width = 160;
+                itemimage.Height = 160;
+
+                Label itemname = new Label();
+                itemname.Content = item.item.name;
+                itemname.Foreground = Brushes.White;
+                itemname.FontSize = 15;
+
+                Border itemframe = new Border();
+                itemframe.BorderThickness = new Thickness(5, 5, 5, 5);
+                var converter = new System.Windows.Media.BrushConverter();
+                if (item.item.rarity == "common") {
+                    itemframe.BorderBrush = (Brush)converter.ConvertFromString("#bebebe");
+                } else if (item.item.rarity == "uncommon") {
+                itemframe.BorderBrush = (Brush)converter.ConvertFromString("#8fee3b");
+                } else if (item.item.rarity == "epic") {
+                    itemframe.BorderBrush = (Brush)converter.ConvertFromString("#b04eec");
+                } else if (item.item.rarity == "rare") {
+                    itemframe.BorderBrush = (Brush)converter.ConvertFromString("#3ad6ff");
+                } else if (item.item.rarity == "legendary") {
+                itemframe.BorderBrush = (Brush)converter.ConvertFromString("#e38748");
+            }
+            itemframe.Margin = new Thickness(5, 5, 0, 0);
+
+                RadialGradientBrush itemtransition = new RadialGradientBrush();
+                if (item.item.rarity == "common") {
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#bebebe"), 0));
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#848282"), 1));
+                } else if (item.item.rarity == "uncommon") {
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#69bb1e"), 0));
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#227622"), 1));
+                } else if (item.item.rarity == "epic") {
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#c359ff"), 0));
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#701fe5"), 1));
+                } else if (item.item.rarity == "rare") {
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#2cc1ff"), 0));
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#215dc2"), 1));
+                } else if (item.item.rarity == "legendary") {
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#ea8d23"), 0));
+                    itemtransition.GradientStops.Add(new GradientStop((Color)ColorConverter.ConvertFromString("#c04d1f"), 1));
+                }
+                itemtransition.Freeze();
+
+                StackPanel itemgroup = new StackPanel();
+                itemgroup.Orientation = Orientation.Vertical;
+                itemgroup.Width = 160;
+                itemgroup.Height = 160;
+                itemgroup.Background = itemtransition;
+                itemgroup.Children.Add(itemname);
+                itemgroup.Children.Add(itemimage);
+
+                itemframe.Child = itemgroup;
+
+                ShopContent.Children.Add(itemframe);
+            }
+            //MessageBox.Show(items.vbucks.ToString());
+            //MessageBox.Show(items.items[0].item.image.ToString());
         }
     }
 }
